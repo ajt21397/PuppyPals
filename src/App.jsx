a0ce2ebd92263f8184afe768797a1824e24acd9f
Adolfo J. Temoche Guevara <ajt21397@hotmail.com>
2023-06-14 20:48:37 -0700

    adding more css elements and changing some js

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import {puppyList} from './data.js'
+import {puppyList} from './data.js' // importing the puppy list from data.js
 import { useState } from 'react'
 import './App.css'
+import './My.css' // importing to My css page
 
 function App() {
   
   const [puppies, setPuppies] = useState(puppyList);
-
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  //console.log(featuredPup);
 
 
   return (
     <>
-      <div>
+      <div className='App'>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+        
     {puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>
+     return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
    })}
       </div>
      
